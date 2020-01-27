11ae16c84dcc3bbb1c07310719d5033dd75b02a1
NicoTaborda <42037379+niico786@users.noreply.github.com>
2020-01-27 00:09:58 -0300

    Habilitado CORS para el servicio get clientes

--- src/main/java/com/udemy/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/udemy/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.udemy.springboot.backend.apirest.models.entity.Cliente;
 import com.udemy.springboot.backend.apirest.models.services.IClienteService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
