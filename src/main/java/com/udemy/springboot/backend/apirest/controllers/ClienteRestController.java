ebb0a368ddfc24bdcdc0141ccc9aa3771df25a4c
NicoTaborda <42037379+niico786@users.noreply.github.com>
2020-05-14 14:35:05 -0300

    Agredao control de errores en delete

--- src/main/java/com/udemy/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/udemy/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -106,9 +106,20 @@ public class ClienteRestController {
 		}
 		
 		@DeleteMapping("/clientes/{id}")
-		@ResponseStatus(HttpStatus.NO_CONTENT)
-		public void delete(@PathVariable Long id) {
-			clienteService.delete(id);
+		public ResponseEntity<?> delete(@PathVariable Long id) {
+			Map<String,Object> response = new HashMap<String, Object>();
+			
+			try {
+				clienteService.delete(id);
+			} catch (DataAccessException e) {
+				response.put("mensaje", "Error al eliminar el cliente en la base de datos!");
+				response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);			
+			}
+			
+			response.put("mensaje", "El cliente ha sido actualizado con exito!");
+			
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 		}
 		
 }
