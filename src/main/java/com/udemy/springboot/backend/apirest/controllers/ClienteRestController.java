5b97cab9113fa7b17786559f55fd0cb7717ada11
NicoTaborda <42037379+niico786@users.noreply.github.com>
2020-05-16 22:52:44 -0300

    Validando con @Valid en los handlers del create y update
    Se implementan dos formas, la que se utiliza es funcional, implementando streams

--- src/main/java/com/udemy/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/udemy/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,13 +1,19 @@
  package com.udemy.springboot.backend.apirest.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -58,9 +64,26 @@ public class ClienteRestController {
 		}
 		
 		@PostMapping("/clientes")
-		public ResponseEntity<?> create (@RequestBody Cliente cliente) {
+		public ResponseEntity<?> create (@Valid @RequestBody Cliente cliente, BindingResult result) {
 			Cliente clienteNew = null;
 			Map<String, Object> response = new HashMap<>();
+			
+			if(result.hasErrors()) {
+			/*
+			 * List<String> errors = new ArrayList<>();
+			 * 
+			 * for(FieldError err: result.getFieldErrors() ) { errors.add("El campo '" +
+			 * err.getField() + "' " + err.getDefaultMessage()); }
+			 */
+				
+				List<String> errors = result.getFieldErrors()
+						.stream()
+						.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+						.collect(Collectors.toList());
+				
+				response.put("errors", errors);
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST); 
+			}
 			
 			try {
 				clienteNew = clienteService.save(cliente);
@@ -76,10 +99,21 @@ public class ClienteRestController {
 		}
 		
 		@PutMapping("/clientes/{id}")
-		public ResponseEntity<?> update (@RequestBody Cliente cliente, @PathVariable Long id) {
+		public ResponseEntity<?> update (@Valid @RequestBody Cliente cliente, BindingResult result, @PathVariable Long id) {
 			Cliente clienteActual = clienteService.findById(id);
 			Cliente clienteUpdated = null;
 			Map<String, Object> response = new HashMap<>();
+			
+			if(result.hasErrors()) {
+				
+					List<String> errors = result.getFieldErrors()
+							.stream()
+							.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+							.collect(Collectors.toList());
+					
+					response.put("errors", errors);
+					return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST); 
+				}
 			
 			if(clienteActual == null) {
 				response.put("mensaje", "Error: no se pudo editar, el cliente ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
