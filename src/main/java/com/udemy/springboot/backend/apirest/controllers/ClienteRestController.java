7b092bf4c7f9f3ec9c501a29105348313b01be87
NicoTaborda <niico786@gmail.com>
2020-09-04 20:57:40 -0300

    Agreagada validacion para mostrar foto cuando no se encuntra la foto

--- src/main/java/com/udemy/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/udemy/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -229,7 +229,13 @@ public class ClienteRestController {
 			}
 			
 			if(!recurso.exists() && !recurso.isReadable()) {
-				throw new RuntimeException("Error: No se pudo cargar la imagen: " + nombreFoto);
+				rutaArchivo = Paths.get("src/main/resources/static/images").resolve("not_user.png").toAbsolutePath();
+				try {
+					recurso = new UrlResource(rutaArchivo.toUri());
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+				log.error("Error: No se pudo cargar la imagen: " + nombreFoto);
 	 		}
 			HttpHeaders cabecera = new HttpHeaders();
 			cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
