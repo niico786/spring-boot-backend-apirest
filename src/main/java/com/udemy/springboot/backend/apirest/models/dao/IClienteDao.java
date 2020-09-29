3eae3ae851838f0742798961ef30ae24e1386f77
NicoTaborda <niico786@gmail.com>
2020-09-29 17:03:00 -0300

    Relacionando tablas: Creando clase entity Region

--- src/main/java/com/udemy/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/udemy/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,7 +1,16 @@
 package com.udemy.springboot.backend.apirest.models.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.udemy.springboot.backend.apirest.models.entity.Cliente;
+import com.udemy.springboot.backend.apirest.models.entity.Region;
 
-public interface IClienteDao extends JpaRepository<Cliente, Long> {}
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
+	
+	@Query("FROM Region")
+	public List<Region> findAllRegiones();
+	
+}
--- src/main/java/com/udemy/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/udemy/springboot/backend/apirest/models/entity/Cliente.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -15,6 +18,9 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="clientes")
@@ -44,6 +50,11 @@ public class Cliente implements Serializable {
 	private Date createAt;
 	
 	private String foto;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "region_id")
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private Region region;
 
 	public long getId() {
 		return id;
@@ -98,10 +109,14 @@ public class Cliente implements Serializable {
 		this.foto = foto;
 	}
 	
-	/**
-	 * 
-	 */
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+
 	private static final long serialVersionUID = 1L;
 	
-
 }
